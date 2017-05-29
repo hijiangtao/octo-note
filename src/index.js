17c4e91934849a7d3bd6d1294fd04b4a30012519
Joe Jiang <hijiangtao@gmail.com>
2017-05-29 22:51:57 +0800

    update: webpack configurations

--- src/index.js
+++ src/index.js
@@ -5,14 +5,25 @@
  * @version $Id$
  */
 
+'use strict'
+
 import g from './global'
 import template from './index.html'
 import './style.css'
+import Vue from 'vue'
 
 g.test = 'test'
 document.title = 'iNote'
 document.body.innerHTML = template
 
 setTimeout(() => {
-  document.getElementById('note').innerHTML = 'Changed.'
+	document.getElementById('note').innerHTML = 'Changed.'
+	// console.log(g)
 }, 3000)
+
+g.ins = new Vue({
+	el: '#test',
+	data: {
+		'we': 'string'
+	}
+})
---
+++ src/vendor.js
@@ -0,0 +1,11 @@
+/**
+ * vendor.js
+ * @authors Joe Jiang (hijiangtao@gmail.com)
+ * @date    2017-05-29 09:50:59
+ * @version $Id$
+ */
+
+import Vue from 'vue'
+
+const we = new Vue({})
+we.a = 'a'
